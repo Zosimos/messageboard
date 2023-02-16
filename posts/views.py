1b9133d6ce97e9c908810bc61480a5ab82ee1842
Jim Barnett <hlbradow@yahoo.com>
2023-02-15 23:05:25 -0500

    class 4

--- posts/views.py
+++ posts/views.py
@@ -1,7 +1,17 @@
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView
+from django.views.generic.edit import CreateView
 from .models import Post
 
 
 class PostListView(ListView):
     template_name = "list.html"
     model = Post
+
+class PostDetailView(DetailView):
+    template_name = "detail.html"
+    model = Post
+
+class PostCreateView(CreateView):
+    template_name = "new.html"
+    model = Post
+    fields = ["title", "subtitle", "body"]
